5385f3d1772371f95c1a8ccac98aba845d732683
Mohammad <mohammadmp7@gmail.com>
2021-04-06 13:34:26 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -8,31 +8,6 @@
   </div>
 </template>
 
-<script>
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-  setup() {
-    
-
-    let maede_porteghali = {
-      name: 'مائده',
-      lastName: 'پرتقالی',
-      riazi: 15,
-      olom: 20,
-      moaddel: 18,
-    }
-
-      maede_porteghali.name = 'مائده'
-      maede_porteghali.riazi = 15
-      maede_porteghali.olom = 20
-
-
-  },
-})
-</script>
-
-
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
